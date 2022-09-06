07349f6a6448b38287d26dedef3d372bf9d02d75
Taha <dominor001@gmail.com>
2022-09-06 14:29:37 +0100

    threex init

--- src/examples/MMM_FINAL/script.js
+++ src/examples/MMM_FINAL/script.js
@@ -3,6 +3,7 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.m
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js";
 import { Rhino3dmLoader } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js";
 import rhino3dm from "https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js";
+import * as THREEx from "./threex.domevents.js";
 
 //import Stats from './jsm/libs/stats.module.js';
 //import { GUI } from './jsm/libs/lil-gui.module.min.js';

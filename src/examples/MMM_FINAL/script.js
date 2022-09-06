82a4fb774c3f32b50cde829091b69168f5905533
Taha <dominor001@gmail.com>
2022-09-06 14:24:15 +0100

    threex init

--- src/examples/MMM_FINAL/script.js
+++ src/examples/MMM_FINAL/script.js
@@ -1,5 +1,5 @@
 /* eslint no-undef: "off", no-unused-vars: "off" */
-//import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js";
 import { Rhino3dmLoader } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js";
 import rhino3dm from "https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js";
